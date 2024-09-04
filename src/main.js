b3d4c20ac3140a5bde4766bf69c3305433dc28c7
elpulpo0 <chris.elpulpo@gmail.com>
2024-09-04 13:40:21 +0200

    update general display

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import VTooltip from "v-tooltip";
 
-// Importer le fichier CSS global
-import "@/styles/main.css";
-
 const app = createApp(App);
 
 app.use(router);
